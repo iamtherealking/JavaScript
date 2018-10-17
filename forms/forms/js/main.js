284ea629183981c358799f842669a1529bdb6a73
pat <35233489+pborlongan@users.noreply.github.com>
2018-10-17 09:50:12 -0600

    intro to forms

---
+++ forms/forms/js/main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+window.addEventListener('load', function(e)
+{
+    const formElements = Array.from(document.querySelectorAll('input')); // "how many things are inside that thing?"
+
+    // for each loop to input console
+    
+    /*formElements.forEach(function(value,index){
+        value.setAttribute("data-index", index);
+        value.addEventListener("click", function(e)
+        {
+            console.log(value);  
+        })
+    })*/
+
+    // map Array.from()
+    
+    // read through stuff, check moodle for more stuff
+})
