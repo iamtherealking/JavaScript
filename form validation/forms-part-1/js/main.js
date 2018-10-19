16470b05f2cd3165e505c0d012e5d035b4afb9a1
Patr <35233489+pborlongan@users.noreply.github.com>
2018-10-19 09:40:20 -0600

    Form validation (with objects)

---
+++ form validation/forms-part-1/js/main.js
@@ -0,0 +1,14 @@
+'use strict'
+window.addEventListener("load", function(e){
+    // const inputFields = document.getElementsByTagName('input')
+    const someElements = Array.from(document.querySelectorAll('input'));
+    
+
+    someElements.map
+
+    document.querySelector('form').addEventListener("submit", function(e){
+        e.preventDefault();
+    })
+
+      
+})
