1c223fa041a5a9cfe512f71304c6193f0352eca0
pat <35233489+pborlongan@users.noreply.github.com>
2018-10-23 12:23:57 -0600

    add

--- assignment2-photo-filter-my-solution/js/main.js
+++ assignment2-photo-filter-my-solution/js/main.js
@@ -4,22 +4,35 @@ window.addEventListener("load", function(e){
     const nodeThumbnail = document.querySelectorAll(".thumb-display");
     const inputTags = document.querySelector(".frm-control")
     
+    let emptyarray = []
+
     nodeThumbnail.forEach(function(elm,index){
         let tempThumbnailObj ={
             id: index,
             tags: elm.querySelector(".tags").innerHTML.replace(/[#]/g, "").split(" ")
         }
-        // set attribute so u can use it
+        elm.dataset.id = tempThumbnailObj.id;
+
         tempThumbnailObj.tags.forEach(function(val){
             let wordTagArray = val.split("");
-            
+            let i = 0;
+            let tempWord = wordTagArray[i];
+
             inputTags.addEventListener("input",function(e){
-                
+                if(tempWord == inputTags.value)
+                {
+                    onDisplay(elm.dataset.id)
+                }
+                else
+                {
+
+                }
             })
         })
-        
-        
-
-
+       emptyarray.push(elm.dataset.id);
     })
+
+    
+
+
 })
