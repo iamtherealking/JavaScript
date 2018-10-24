e14a9ac6095484ddd2cfcedf2e9e73392dd116f8
pat <35233489+pborlongan@users.noreply.github.com>
2018-10-24 11:41:16 -0600

    Updated Assignment

--- assignment2-photo-filter-my-solution/js/main.js
+++ assignment2-photo-filter-my-solution/js/main.js
@@ -2,32 +2,25 @@
 
 window.addEventListener("load", function(e){
     const nodeThumbnail = document.querySelectorAll(".thumb-display");
-    const inputTags = document.querySelector(".frm-control")
-
-    /*
+    const inputTags = document.querySelector(".frm-control");
+    
     nodeThumbnail.forEach(function(elm,index){
-        let tempThumbnailObj ={
-            id: index,
-            tags: elm.querySelector(".tags").innerHTML.replace(/[#]/g, "").split(" ")
-        }
-        elm.dataset.id = tempThumbnailObj.id;
-        elm.dataset.tags = tempThumbnailObj.tags;
-
+        let tagsArray = elm.querySelector(".tags").innerHTML.replace(/[#]/g, "").split(" ")
 
         inputTags.addEventListener("input", function(e){
-            let tempWord = "";
-            let i = 0;
-            tempThumbnailObj.tags.forEach(function(val){
+            
+            tagsArray.forEach(function(val){
                 let wordTagArray = val.split("");
+                let i = 0;
                 tempWord = wordTagArray[i];
                 if(tempWord == inputTags.value)
                 {
-                    nodeThumbnail[index].classList.toggle("hidden")
+                   console.log("oof")
                 }
             })
         })
     })
-    */
+
 
 
 })
