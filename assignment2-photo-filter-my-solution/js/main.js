2339fb044b45d52a70bd59515f538b2c65f37498
pat <35233489+pborlongan@users.noreply.github.com>
2018-10-30 13:05:40 -0600

    DONEEEEEEE

--- assignment2-photo-filter-my-solution/js/main.js
+++ assignment2-photo-filter-my-solution/js/main.js
@@ -11,21 +11,32 @@ window.addEventListener("load", function(e){
             //filter
             let inputComp = String(inputTags.value);
             let inputlength = inputComp.length;
-            nodeThumbnail[index].classList.add("hidden")
+
+            nodeThumbnail[index].classList.add("hidden");
             let tags = tagsArray.filter(function(value,ind){
                 if(inputTags.value == value.substring(0,inputlength))
                 {
                     nodeThumbnail[index].classList.remove("hidden")
                 }
             })
-        })
 
-        //let newArray = inventors.filter(function(inv,ind){
-            //    const firstLetter = inv.first.charAt(0).toLowerCase();
-            //    if(firstLetter === "a"){
-            //        return inv
-            //    }
-            //})
+            if(inputTags.value !== "")
+            {
+                let resetClick = document.querySelector(".reset");
+                resetClick.classList.remove("hidden");
+
+                resetClick.addEventListener("click",function(e){
+                    inputTags.value = ""
+                    nodeThumbnail[index].classList.remove("hidden")
+
+                    if(inputTags.value == "")
+                    {
+                        document.querySelector(".reset").classList.add("hidden");
+                    }
+                })
+
+            }
+        })
     })
 
 
