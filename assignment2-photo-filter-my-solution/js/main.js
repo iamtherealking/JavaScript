584b2b3dcb9cabbfbceae29728049e49ddfb32be
pat <35233489+pborlongan@users.noreply.github.com>
2018-10-29 21:18:35 -0600

    jhabjfhbadkjafd i got it
    just needs the reset

--- assignment2-photo-filter-my-solution/js/main.js
+++ assignment2-photo-filter-my-solution/js/main.js
@@ -3,16 +3,29 @@
 window.addEventListener("load", function(e){
     const nodeThumbnail = document.querySelectorAll(".thumb-display");
     const inputTags = document.querySelector(".frm-control");
-    
+
     nodeThumbnail.forEach(function(elm,index){
-        let tagsArray = elm.querySelector(".tags").innerHTML.replace(/[#]/g, "").split(" ")
+        let tagsArray = elm.querySelector(".tags").innerHTML.replace(/[#]/g, "").split(" ");
 
         inputTags.addEventListener("input", function(e){
             //filter
-            tagsArray.forEach(function(val){
-                
+            let inputComp = String(inputTags.value);
+            let inputlength = inputComp.length;
+            nodeThumbnail[index].classList.add("hidden")
+            let tags = tagsArray.filter(function(value,ind){
+                if(inputTags.value == value.substring(0,inputlength))
+                {
+                    nodeThumbnail[index].classList.remove("hidden")
+                }
             })
         })
+
+        //let newArray = inventors.filter(function(inv,ind){
+            //    const firstLetter = inv.first.charAt(0).toLowerCase();
+            //    if(firstLetter === "a"){
+            //        return inv
+            //    }
+            //})
     })
 
 
